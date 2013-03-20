9e4c2eb58221caf6dd96b6ec85094fc5962ea2bf
Farida <fakhtar@A01C00490.ad.sogeti.se>
2013-03-20 12:55:42 +0100

    first commit

---
+++ Java_farida/src/faridasp/math.java
@@ -0,0 +1,12 @@
+package faridasp;
+
+public class math {
+	public static void main (String [] args){
+		int x=8,y=7;
+		boolean comp = x != y;
+		System.out.print(comp);
+		
+		
+	}
+
+}
---
+++ Java_farida/src/faridasp/mathTest.java
@@ -0,0 +1,28 @@
+package faridasp;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class mathTest {
+
+
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+
+
+	@Test
+	public void testMain() {
+		int x=8,y=7;
+		boolean comp = x != y;
+		System.out.print(comp);
+	}
+
+}
