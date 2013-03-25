7a4ffa99fa67cf1b8988fb5a4db1cca3e5bc2e4e
Farida <fakhtar@A01C00490.ad.sogeti.se>
2013-03-25 10:39:34 +0100

    idag253

---
+++ Java_farida/src/faridasp/Minaarrayclass.java
@@ -0,0 +1,13 @@
+package faridasp;
+
+public class Minaarrayclass {
+	public static void main (String[] args){
+	String[] names= {"Emma","Lotta","Ebba"};
+	
+	
+	System.out.println("Hejsan "+ names[0]);
+	System.out.println("Hejsan "+ names[1]);
+	
+	}
+
+}
