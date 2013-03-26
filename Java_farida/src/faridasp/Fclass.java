62f74cb728c4f1188b043b41b8262519d0cc9b11
Farida <fakhtar@A01C00490.ad.sogeti.se>
2013-03-26 11:01:08 +0100

    first today

---
+++ Java_farida/src/faridasp/Fclass.java
@@ -0,0 +1,12 @@
+package faridasp;
+
+class Fclass {
+	public void plus(int x,int y){
+		System.out.println(x);
+		System.out.println(y);
+		
+	
+	
+	}	
+
+}
--- Java_farida/src/faridasp/Myclass.java
+++ Java_farida/src/faridasp/Myclass.java
@@ -3,13 +3,16 @@ package faridasp;
 public class Myclass {
 	public static void main(String [] args){
 		
-		String[] names=new String[3];
-		names[0]="Asha";
-		names[1]="Nisha";
-		names[2]="Frida";
-		 for(String name:names){
-			System.out.println("Hallå "+ name);
-		}
+		
+		Fclass ref = new Fclass();
+		
+		
+		ref.plus(8,9);
+		
+		
+		
+		
+		
 			
 	}
 }
